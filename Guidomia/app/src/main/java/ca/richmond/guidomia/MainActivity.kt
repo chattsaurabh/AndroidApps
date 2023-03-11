9c864000ce89a164ab1dfcac0665d76ccf932322
Saurabh <saurabh@radical.io>
2023-03-11 08:54:09 -0500

    Guidomia - Added list and adapter skeleton.

--- Guidomia/app/src/main/java/ca/richmond/guidomia/MainActivity.kt
+++ Guidomia/app/src/main/java/ca/richmond/guidomia/MainActivity.kt
@@ -1,11 +1,25 @@
 package ca.richmond.guidomia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import ca.richmond.guidomia.adapters.GuidomiaListAdapter
+import ca.richmond.guidomia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupList()
+    }
+
+    private fun setupList() {
+        var list = binding.recyclerView
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter =
+            GuidomiaListAdapter()
     }
 }
